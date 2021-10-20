26722fd9c404fa886dda8395c230a1848dae4b22
jagwarthegreat <jguapin21@gmail.com>
2021-10-20 16:48:35 +0800

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
 {
     public function index(Request $request)
     {
-        $post = Post::where('class_id', $request->class_id)->orderByDesc('created_at')->with('user', 'postAttachments', 'postComments')->get();
+        $post = Post::where('class_id', $request->class_id)->orderByDesc('created_at')->with('user', 'postAttachments')->get();
         return response()->json($post);
     }
 
