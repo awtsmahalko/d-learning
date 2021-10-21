9bc45c8c5df97c188bb29656c91fc61f21be7f4a
jagwarthegreat <jguapin21@gmail.com>
2021-10-21 15:26:50 +0800

    Update MeetingController.php

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use ZoomApiController;
+use App\Http\Controllers\ZoomApiController;
+
 class MeetingController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         $data = ZoomApiController::createZoomMeeting();
         return response()->json($data);
     }
