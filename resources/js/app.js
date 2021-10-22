da0450172d1f0457d5051ce5cfb1dfc311d6b56e
awtsmahalko <56856915+awtsmahalko@users.noreply.github.com>
2021-10-22 15:23:26 +0800

    rin init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,24 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import Vue from 'vue'
+import Vue from "vue";
 
-import App from './component/app';
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './routes';
+import App from "./component/app";
+import VueRouter from "vue-router";
+import VueAxios from "vue-axios";
+import axios from "axios";
+import { routes } from "./routes";
 
 Vue.use(VueRouter);
-Vue.use(VueAxios,axios);
+Vue.use(VueAxios, axios);
 
 const router = new VueRouter({
-    mode:'history',
-    routes:routes,
-    linkActiveClass: "active",
-})
+    mode: "history",
+    routes: routes,
+    linkActiveClass: "active"
+});
 
 const app = new Vue({
-    el: '#app',
-    router:router,
-    render: h => h(App),
+    el: "#app",
+    router: router,
+    render: h => h(App)
 });
