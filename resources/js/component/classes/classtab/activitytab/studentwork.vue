299f30d55f3b2cb11f921b652b9511760c0b5e93
jagtheripper <jguapin21@gmail.com>
2021-12-09 13:28:52 +0800

    fix avatars

--- resources/js/component/classes/classtab/activitytab/studentwork.vue
+++ resources/js/component/classes/classtab/activitytab/studentwork.vue
@@ -344,7 +344,7 @@ export default {
     },
     assetPP(path) {
       var thumbs =
-        path == ""
+        path == null
           ? imgUrl + "/profile/default/default_avatar.jpeg"
           : imgUrl + path;
       return thumbs;
